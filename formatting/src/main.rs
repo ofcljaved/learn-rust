7a840d1b0e2a31579014d3fbb9b1f63919facaf0
ofcljaved <javedofficialme@gmail.com>
2024-10-07 13:26:37 +0530

    chore: format decimals

---
+++ formatting/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    print!("This will not add new line at the end");
+    println!("testing new line");
+    println!("This will add new line");
+    println!("testing new line");
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}",  pi);
+}
