202d0bd32cfa77e15a68bb155b0f68334b13bf63
ofcljaved <javedofficialme@gmail.com>
2024-10-25 18:11:24 +0530

    chore: spawn task

--- redis/src/main.rs
+++ redis/src/main.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -15,12 +17,29 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use std::collections::HashMap;
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                }
+                else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        let response = Frame::Error("unimplemeted".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
