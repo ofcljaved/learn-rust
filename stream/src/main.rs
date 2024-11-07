30f540cc6d586abe5eac23384b828c907e528d05
ofcljaved <javedofficialme@gmail.com>
2024-11-07 23:48:49 +0530

    chore: error handling in stream

--- stream/src/main.rs
+++ stream/src/main.rs
@@ -4,9 +4,15 @@ use trpl::{ReceiverStream, Stream, StreamExt};
 
 fn main() {
     trpl::run(async {
-        let mut messages = pin!(get_messages().timeout(Duration::from_millis(200)));
+        let messages = get_messages().timeout(Duration::from_millis(200));
+        let intervals = get_intervals()
+            .map(|x| format!("Interval {x}"))
+            .throttle(Duration::from_millis(100))
+            .timeout(Duration::from_secs(10));
+        let merged = messages.merge(intervals).take(20);
+        let mut stream = pin!(merged);
 
-        while let Some(result) = messages.next().await {
+        while let Some(result) = stream.next().await {
             match result {
                 Ok(message) => println!("{message}"),
                 Err(reason) => eprintln!("Problem :{reason}"),
@@ -23,7 +29,27 @@ fn get_messages() -> impl Stream<Item = String> {
         for (index, message) in messages.into_iter().enumerate() {
             let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
             trpl::sleep(Duration::from_millis(time_to_sleep)).await;
-            tx.send(format!("Message: {message}")).unwrap();
+            if let Err(send_error) = tx.send(format!("Message: {message}")) {
+                eprintln!("Cannot send messages '{message}': {send_error}");
+                break;
+            }
+        }
+    });
+    ReceiverStream::new(rx)
+}
+
+fn get_intervals() -> impl Stream<Item = u32> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let mut count = 0;
+        loop {
+            trpl::sleep(Duration::from_millis(1)).await;
+            count += 1;
+            if let Err(send_error) = tx.send(count) {
+                eprintln!("Cannot increase count, {send_error}");
+                break;
+            }
         }
     });
     ReceiverStream::new(rx)
