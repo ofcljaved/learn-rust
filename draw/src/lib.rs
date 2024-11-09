3e4f21f575f397fa2867c27e3a3e1a0a525c5fd2
ofcljaved <javedofficialme@gmail.com>
2024-11-09 09:06:50 +0530

    chore: trait object

---
+++ draw/src/lib.rs
@@ -0,0 +1,42 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        //here will be the code okauuu
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
