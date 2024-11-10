0a5acdb65bffa2a0b0d2dc3f34b723998103034d
ofcljaved <javedofficialme@gmail.com>
2024-11-11 02:11:41 +0530

    feat: add multithreading

--- http/src/main.rs
+++ http/src/main.rs
@@ -2,32 +2,37 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use http::ThreadPool;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream));
+        //handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    /*
-        let http_request: Vec<_> = buf_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
-    */
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+
+    let (status, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_millis(5000));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
